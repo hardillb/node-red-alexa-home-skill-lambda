ac30eaa4a6d658ed3fa4d8802fba8c8163e295c1
Ben Hardill <hardillb@gmail.com>
2016-11-06 19:01:06 +0000

    Now flags expired tokens

--- index.js
+++ index.js
@@ -1,73 +1,82 @@
 var request = require('request');
 
-exports.myHandler = function(event, context, callback) {
+exports.handler = function(event, context) {
+    log("Entry", event);
     if (event.header.namespace === 'Alexa.ConnectedHome.Discovery') {
-        discover(event, context, callback);
+        discover(event, context);
     } else if (event.header.namespace === 'Alexa.ConnectedHome.Control') {
-        control(event,context, callback)
+        command(event,context)
     } else if (event.header.namespace === 'Alexa.ConnectedHome.System') {
-        system(event,context, callback);
+        system(event,context);
     }
 };
 
-function discover(event, context, callback) {
+function discover(event, context) {
+    log("Discover", event);
     if (event.header.name === 'DiscoverAppliancesRequest') {
         var message_id = event.header.messageId;
         var oauth_id = event.payload.accessToken;
+        
 
         //http request to the database
-        request.get('',{
+        request.get('https://alexa-node-red.eu-gb.mybluemix.net/api/v1/devices',{
             auth: {
                 'bearer': oauth_id
             },
             timeout: 2000
-        },function(err){
-            //timeout error?
-        }).on('response',function(response){
+        },function(err, response, body){
+            log("Discover body", body);
+            if (response.statusCode == 200) {
+                var payload = {
+                    discoveredAppliances: JSON.parse(body)
+                };
 
-        })on('error', function(error){
+                var response = {
+                    header:{
+                        messageId: message_id,
+                        name: "DiscoverAppliancesResponse",
+                        namespace: "Alexa.ConnectedHome.Discovery",
+                        payloadVersion: "2"
+                    },
+                    payload: payload
+                };
+
+                log('Discovery', response);
+
+                context.succeed(response);
+            } else if (response.statusCode == 401) {
+                log('Discovery', "Auth failure");
+                var response = {
+                    header:{
+                        messageId: message_id,
+                        namespace: "Alexa.ConnectedHome.Control",
+                        name: "ExpiredAccessTokenError",
+                        payloadVersion: "2"
+                    },
+                    payload:{}
+                };
+    
+                context.succeed(response);
+            }
+
+        }).on('error', function(error){
+            log('Discovery',"error: " + error);
             //other error
+
+
+            context.fail(error);
         });
-
-        var payload = {
-            discoveredAppliances:[
-                {
-                    applianceId: "",
-                    manufacturerName: "Node-RED";
-                    modelName: "",
-                    version: "0.0.1",
-                    friendlyName: "Bedroom Light",
-                    friendlyDescription: "Lights in Bedroom connected via Node-RED",
-                    isReachable: true,
-                    actions: [
-                        "setPercentage",
-                        "turnOn",
-                        "turnOff"
-                    ],
-                    additionalApplianceDetails: {}
-                }
-            ]
-        };
-
-        var response = {
-            header:{
-                "messageId": message_id,
-                "name": "DiscoverAppliancesResponse",
-                "namespace": "Alexa.ConnectedHome.Discovery",
-                "payloadVersion": "2"
-            },
-            payload: payload
-        };
-
-        callback(null, response);
     }
 }
 
-function command(event, context, callback) {
+function command(event, context) {
     var device_id = event.payload.appliance.applianceId;
     var message_id = event.header.messageId;
+    var oauth_id = event.payload.accessToken;
 
     var command = event.header.name;
+
+    log("Command", event);
 
     var header = {
             namespace: "Alexa.ConnectedHome.Control",
@@ -76,36 +85,81 @@ function command(event, context, callback) {
         }
 
     switch(command) {
-        case "TurnOnRequest":
-            header.name = "TurnOnConfirmation"
+        case 'TurnOnRequest':
+            header.name = "TurnOnConfirmation";
             break;
-        case "TurnOffRequest":
+        case 'TurnOffRequest':
             header.name = "TurnOffConfirmation"
             break;
-        case "SetPercentageRequest":
-            header.name = "SetPercentageConfirmation";
+        case 'SetTargetTemperatureRequest':
+            header.name = "SetTargetTemperatureConfirmation"
             break;
-        case "IncrementPercentageRequest":
+        case 'IncrementTargetTemperatureRequest':
+            header.name = "IncrementTargetTemperatureConfirmation";
+            break;
+        case 'DecrementTargetTemperatureRequest':
+            header.name = "DecrementTargetTemperatureConfirmation";
+            break;
+        case 'SetPercentageRequest':
+            header.name = "SetPercentageConfirmation"
+            break;
+        case 'IncrementPercentageRequest':
             header.name = "IncrementPercentageConfirmation";
             break;
-        case "DecrementPercentageRequest"
+        case 'DecrementPercentageRequest':
             header.name = "DecrementPercentageConfirmation";
             break;
     }
 
-    response = {
-        header: header,
-        payload: {}
-    }
-    callback(null,response);
+    request.post('https://alexa-node-red.eu-gb.mybluemix.net/api/v1/command',{
+        json: event,
+        auth: {
+            bearer: oauth_id
+        },
+        timeout: 2000
+    }, function(err, resp, data){
+        if (resp.statusCode === 200) {
+            var response = {
+                header: header,
+                payload: {
+                }
+            }
+            context.succeed(response);
+        } else if (resp.statusCode === 401) {
+            log('command', "Auth failure");
+            var response = {
+                header:{
+                    messageId: message_id,
+                    namespace: "Alexa.ConnectedHome.Control",
+                    name: "ExpiredAccessTokenError",
+                    payloadVersion: "2"
+                },
+                payload:{}
+            };
+    
+            context.succeed(response);
+        }
+
+        
+    }).on('errror', function(){
+        header.name="TargetOfflineError"
+        var  response = {
+            header: header,
+            payload: {
+            }
+        }
+        log("Command",response);
+        context.fail(response);
+    });
+
 }
 
-function system(event, context, callback) {
+function system(event, context) {
     var message_id = event.header.messageId;
 
     var response = {
         "header": {
-            "messageId": messageId,
+            "messageId": message_id,
             "name": "HealthCheckResponse",
             "namespace": "Alexa.ConnectedHome.System",
             "payloadVersion": "2"
@@ -115,5 +169,11 @@ function system(event, context, callback) {
             "isHealthy": true
         }
     };
-    callback(null,response);
+    context.succeed(response);
+}
+
+function log(title, msg) {
+    console.log('*************** ' + title + ' *************');
+    console.log(msg);
+    console.log('*************** ' + title + ' End*************');
 }
